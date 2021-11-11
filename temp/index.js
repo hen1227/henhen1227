093f630acf3f977f5cfeb78a123c03f1812de8bf
Henry Abrahamsen <32469043+hen1227@users.noreply.github.com>
2021-11-11 17:16:51 -0500

    Update index.js

--- temp/index.js
+++ temp/index.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
