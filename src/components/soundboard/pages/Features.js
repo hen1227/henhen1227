0419a8d1970675610b9455819de7f9f3d293aa5e
hen1227 <henhen1227@gmail.com>
2022-08-05 18:25:11 -0400

    Add Meta Tags

--- src/components/soundboard/pages/Features.js
+++ src/components/soundboard/pages/Features.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import '../../../App.css';
 import '../Features.css';
-import FeatureItem from '../FeatureItem'
+import FeatureItem from '../FeatureItem';
+import {Button} from '../../Button';
+import Head from './Head'
 
 
 export default function Features() {
   return <>
+    <Head/>
   <div className='features'>
-        <h1 style={{color:"#eee"}}>KeySoundboard Features</h1>
+        <div className='description'>
+        <h1 style={{color:"#fff"}}>KeySoundboard</h1>
+        <br/>
+          <p>KeySoundboard allows you to play sound files at the press of a button. On ANY window!</p>
+        </div>
+
         <div className='features__container'>
             <ul className='features__items'>
         <FeatureItem
         src={'/soundboard/images/soundControl.png'}
         alt="Sound Control"
-        text='Control the sound effects reverb, volume, and play speed'
+        text='Control the sound effects speed, reverb, and volume'
         />
         <FeatureItem
         src={'/soundboard/images/keybinds.png'}
         alt="Keybinds"
         text='Set sounds to play on any keybind'
+        />
+        <FeatureItem
+        src={'/soundboard/images/keypad.jpg'}
+        alt="Keypad"
+        text='Fully compatible with numpads too!'
         />
         <FeatureItem
         src={'/soundboard/images/MLGHorn.png'}
@@ -27,6 +40,15 @@ export default function Features() {
         />
         </ul>
       </div>
+      <Button
+            className='btns'
+            buttonStyle='btn--primary'
+            buttonSize='btn--large'
+            localDestination={false}
+            destination='https://apps.apple.com/us/app/keysoundboard/id1636744123'
+          >
+            VIEW ON APP STORE
+      </Button>
       </div>
   </>
 }
--- src/components/soundboard/pages/Home.js
+++ src/components/soundboard/pages/Home.js
@@ -2,10 +2,13 @@ import React from 'react';
 import '../../../App.css';
 import Sounds from '../Sounds';
 import HeroSection from '../HeroSection';
+import MetaTags from 'react-meta-tags'
+import Head from './Head'
 
 function Home() {
   return (
     <>
+      <Head/>
       <HeroSection />
       <Sounds/>
     </>
