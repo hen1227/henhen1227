1948e6ef70afdb8e8c84e95fccca96706a31f93c
hen1227 <henhen1227@gmail.com>
2022-08-08 15:06:57 -0400

    Added a little content to Home page

--- src/components/main/pages/Home.js
+++ src/components/main/pages/Home.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import '../../../App.css';
-
+import '../Home.css'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { useSpring, animated, config } from "@react-spring/web";
 
 function Home() {
   return (
     <>
-        
-        <p>Welcome to henhen1227</p>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        
+      <Parallax pages={1} className="MainParallax">
+        <div className='MainView'>
+        <ParallaxLayer speed={2} offset={0.2}>
+          <h1>Land of Henhen1227</h1>
+        </ParallaxLayer>
+        <ParallaxLayer speed={2} offset={0.4}>
+          <h2>Whats next?</h2>
+          <h3>Could be anything</h3>
+        </ParallaxLayer>
+        <ParallaxLayer speed={1} offset={0.9}>
+          <p>Coming soon</p>
+        </ParallaxLayer>
+        </div>
+      </Parallax>
+
+
+
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     </>
   );
 }
