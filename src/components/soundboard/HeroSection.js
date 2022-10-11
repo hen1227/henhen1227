9a4de0e4969b72c3f25fd7b67affb9149b10e4f7
hen1227 <henhen1227@gmail.com>
2022-10-11 17:41:45 -0400

    Updates

--- src/components/soundboard/HeroSection.js
+++ src/components/soundboard/HeroSection.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../App.css';
 import { Button } from '../Button';
 import './HeroSection.css';
+import axios from "axios";
 
 
 function HeroSection() {
+
+    // const [rating, setRating] = useState(-1)
+
+    // axios({
+    //     method: "GET",
+    //     mode: "no-cors",
+    //     headers: {
+    //         "alg": "ES256",
+    //         "kid": "B6QM5L5S9L",
+    //         "typ": "JWT"
+    //     },
+    //     url: 'https://api.appstoreconnect.apple.com/v1/apps/id1636744123/customerReviews'
+    //     }
+    // )
+    //     .then(function (result){
+    //         console.log(result)
+    //         return result
+    //     })
+
+
   return (
       <div>
         <div className='hero-container'>
@@ -30,6 +51,7 @@ function HeroSection() {
             SEE FEATURES
           </Button>
         </div>
+            {/*<p><span className="ratingLbl" style={{color: "gold"}}>★ </span><span className="ratingLbl">{rating === -1? "loading" :  rating}</span></p>*/}
       </div> 
       </div>
   );
--- src/components/soundboard/SoundItem.js
+++ src/components/soundboard/SoundItem.js
@@ -5,12 +5,12 @@ function SoundItem(props) {
   return (
     <>
       <li className='cards__item'>
-        <Link className='cards__item__link'>
-          <iframe width="120" height="100" src={props.sound} frameborder="0" scrolling="no"></iframe>
+        <div className='cards__item__link'>
+          <iframe width="120" height="100" src={props.sound} frameBorder="0" scrolling="no"></iframe>
           <div className='cards__item__info'>
             <h5 className='cards__item__text'>{props.label}</h5>
           </div>
-          </Link>
+          </div>
       </li>
       
     </>
