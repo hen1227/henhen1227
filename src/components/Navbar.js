92730c278d3baec37e02716acfcf5f511b2b0e01
hen1227 <henhen1227@gmail.com>
2022-10-11 20:00:01 -0400

    Added the initial Braille Decoded page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+  // const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
+    // if (window.innerWidth <= 960) {
+    //   setButton(false);
+    // } else {
+    //   setButton(true);
+    // }
   };
 
   useEffect(() => {
