ec5fc814b283b5f6fca6ca06a91c211fee3caa3a
hen1227 <henhen1227@gmail.com>
2022-10-30 15:39:13 -0400

    Updated the Mushroom page

--- src/components/mushroom/Body.js
+++ src/components/mushroom/Body.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import '../../App.css';
 import './Body.css'
 
@@ -15,7 +15,8 @@ function Body() {
 
 
                 <h1>Game Engine</h1>
-                <h2>Mushroom</h2>
+                <br/>
+                <h2>ⰔMushroom</h2>
                 <br/>
                 <br/>
                 <p><b>Why Mushroom as a name?</b></p>
@@ -25,27 +26,24 @@ function Body() {
                 <br/>
                 <br/>
 
+                <GamePlayImage
+                    title="Gameplay"
+                    message="An explorable platformer"
+                    image="mushroomImage1"
+                />
 
-                <h3>Gameplay</h3>
-                <p>An explorable platformer </p>
-                <div className={"gamePlayImage mushroomImage1"}/>
+                <GamePlayImage
+                    title="Multiplayer"
+                    message="A mix of both UDP and TCP connections to keep the game under 10kbs while keeping it run smoothly"
+                    image="mushroomImage2"
+                /><p>Image soon!</p>
 
+                <GamePlayImage
+                    title="Level Editor"
+                    message="A built in level editor makes it possible to saves and load levels to local device"
+                    image="mushroomImage3"
+                /><p>Image soon!</p>
 
-                <h3>Multiplayer</h3>
-                <p>A mix of both UDP and TCP connections to keep the game under 10kbs while keeping it run smoothly</p>
-                <div className={"gamePlayImage mushroomImage2"}>
-                    <p><b>Image Soon</b></p><br/>
-                    <p>I accidentally broke multiplayer capability</p>
-                    <p>while adding font rendering and updating the UI</p>
-                </div>
-
-
-                <h3>Level Editor</h3>
-                <p>A built in level editor makes it possible to saves and load levels to local device</p>
-                <div className={"gamePlayImage mushroomImage2"}>
-                    <p><b>Image Soon</b></p><br/>
-                    <p>This is currently broken too :(</p>
-                </div>
                 {/*<h3>Capabilities</h3>*/}
                 {/*<br/>*/}
                 {/*<ul>*/}
@@ -59,6 +57,38 @@ function Body() {
 
             </div>
         </div>
+    );
+}
+
+function GamePlayImage(props){
+
+    const [fullscreen, setFullscreen] = useState(" fullscreen left");
+
+    const titleRef = useRef(null);
+
+    function handleClick(){
+        if(fullscreen !== " fullscreen"){
+            // window.scrollTo({
+            //     top: titleRef.current.offsetTop
+            // });
+            setFullscreen(" fullscreen");
+        }else{
+            // window.scrollTo({
+            //     top: titleRef.current.offsetTop
+            // });
+            setFullscreen(" fullscreen left");
+        }
+    }
+
+    return(
+        <>
+            <div>
+                <h3 ref={titleRef}>{props.title}</h3>
+                <p>{props.message}</p>
+                <div onClick={handleClick} className={"gamePlayImage "+props.image}/>
+                <div onClick={handleClick} className={"gamePlayImageFullscreen "+props.image + fullscreen}/>
+            </div>
+        </>
     );
 }
 
