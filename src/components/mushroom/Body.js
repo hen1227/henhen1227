037e71445ac3c4ed280ef55eb8f5d04110fd893d
hen1227 <henhen1227@gmail.com>
2022-11-01 13:52:04 -0400

    Added images to mushroom Page and updated some of its elements

--- src/components/mushroom/Body.js
+++ src/components/mushroom/Body.js
@@ -6,88 +6,59 @@ function Body() {
     return (
         <div>
             <div className='mushroom'>
-
                 <div className='projectOverview'>
                     <h1>Project Overview</h1>
                     <h4>Oct 2022</h4>
                     <div className={"projectOverviewImage"}/>
                 </div>
-
-
-                <h1>Game Engine</h1>
-                <br/>
-                <h2>Mushroom</h2>
+                <h1>Mushroom</h1>
                 <br/>
                 <br/>
                 <p><b>Why Mushroom as a name?</b></p>
                 <p>That's easy, there is mush room for improvement</p>
-                <p>This is my first project in Java... ever</p>
-
+                <p>This is my first project in Java... ever.</p>
                 <br/>
                 <br/>
-
                 <GamePlayImage
                     title="Gameplay"
                     message="An explorable platformer"
                     image="mushroomImage1"
                 />
-
                 <GamePlayImage
                     title="Multiplayer"
                     message="A mix of both UDP and TCP connections to keep the game under 10kbs while keeping it run smoothly"
                     image="mushroomImage2"
                 /><p>Image soon!</p>
-
                 <GamePlayImage
                     title="Level Editor"
                     message="A built in level editor makes it possible to saves and load levels to local device"
                     image="mushroomImage3"
                 /><p>Image soon!</p>
-
-                {/*<h3>Capabilities</h3>*/}
-                {/*<br/>*/}
-                {/*<ul>*/}
-                {/*    <li>Texture Rendering</li>*/}
-                {/*    <li>Font Rendering (soon)</li>*/}
-                {/*    <li>2D Platformer</li>*/}
-                {/*    <li>2D Platformer</li>*/}
-                {/*    */}
-
-                {/*</ul>*/}
-
             </div>
         </div>
     );
 }
 
 function GamePlayImage(props){
-
-    const [fullscreen, setFullscreen] = useState(" left");
+    const [side, setSide] = useState("left");
     const [left, setLeft] = useState(false);
 
     const titleRef = useRef(null);
 
     function handleZoomClick(){
-        console.log(fullscreen);
-        if(fullscreen === " left"){
-            console.log("was on left")
+        if(side === "left"){
             setLeft(false);
-        }else if(fullscreen === " right"){
-            console.log("was on right")
+        }else if(side === "right"){
             setLeft(true);
         }
-        setFullscreen("");
-        console.log("Zoomed");
+        setSide("");
     }
     function handleCloseClick(){
-        console.log("Closed");
-        console.log(left);
         if(left){
-            setFullscreen(" left");
+            setSide("left");
         }else{
-            setFullscreen(" right");
+            setSide("right");
         }
-
         // window.scrollTo({
         //     top: titleRef.current.offsetTop
         // });
@@ -99,7 +70,7 @@ function GamePlayImage(props){
                 <h3 ref={titleRef}>{props.title}</h3>
                 <p>{props.message}</p>
                 <div onClick={handleZoomClick} className={"gamePlayImage "+props.image}/>
-                <div onClick={handleCloseClick} className={"gamePlayImageFullscreen "+props.image + " fullscreen" + fullscreen}/>
+                <div onClick={handleCloseClick} className={"gamePlayImageFullscreen "+props.image + " fullscreen " + side}/>
             </div>
         </>
     );
