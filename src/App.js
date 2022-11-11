c521eec6a33d32f498a98290f1c1bb9dd0bc88eb
hen1227 <henhen1227@gmail.com>
2022-11-11 15:35:59 -0500

    Added App Icon and title. Fixed buttons not aligned properly on mobile devices

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ import Morse from './components/morse/pages/Home'
 import MorseFeatures from './components/morse/pages/Features'
 import Soundboard from './components/soundboard/pages/Home';
 import SoundboardFeatures from './components/soundboard/pages/Features';
-import Fungo from './components/fungo/pages/Home'
+// import Fungo from './components/fungo/pages/Home'
 import Mushroom from './components/mushroom/pages/Home'
 import Home from './components/main/pages/Home'
+import Overview from './components/main/overview/Home'
 import Info from './components/main/pages/Info'
 import Support from './components/main/pages/Support'
 import Privacy from './components/main/pages/privacy'
@@ -21,10 +22,26 @@ import TermsOfService from './components/main/pages/terms-of-service.js'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import PageNotFound from './components/main/pages/PageNotFound'
 
+import {Helmet} from "react-helmet";
+
 function App() {
   return (
     <>
       <Router>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Henhen1227</title>
+          <meta name="This is a site with all the projects and products from Henhen1227" content="Learn more about the awesome stuff at Henhen1227" />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/main/icons/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/main/icons/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/main/icons/favicon-16x16.png"/>
+          <link rel="manifest" href="/main/icons/site.webmanifest"/>
+          <link rel="shortcut icon" href="/main/icons/favicon.ico"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
+          <meta name="msapplication-config" content="/main/icons/browserconfig.xml"/>
+          <meta name="theme-color" content="#202020"/>
+        </Helmet>
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
@@ -43,8 +60,9 @@ function App() {
           <Route path='/soundboard' exact component={Soundboard} />
           <Route path='/soundboard/features' component={SoundboardFeatures} />
           <Route path='/soundboard/support' exact>{<Redirect to="/support" />}</Route>
-          <Route path='/fungo' exact component={Fungo} />
+          <Route path='/fungo' exact component={Mushroom} />
           <Route path='/mushroom' exact component={Mushroom} />
+          <Route path='/overview' exact component={Overview} />
           <Route path="*" component={PageNotFound} />
         </Switch>
         <Footer />
---
+++ src/components/main/overview/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../../App.css';
+import './Home.css'
+
+
+function Home() {
+    return (
+        <>
+            <div className="Overview">
+                <h1>Overview</h1>
+            </div>
+        </>
+    );
+}
+
+export default Home;
