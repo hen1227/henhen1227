5a22453bd764c0b6aac4ebc649f3245c85fcb112
hen1227 <henhen1227@gmail.com>
2022-11-27 17:55:54 -0500

    Clicking on Project goes to relevant page

--- src/components/main/overview/Home.js
+++ src/components/main/overview/Home.js
@@ -8,15 +8,15 @@ import SearchBar from './SearchBar'
 // import Tag from "./Tag";
 
 const projects = {
-    "This website":{"description":"Hello world!","date":"August 2022 - Now","image":"/main/images/logoDark.png","tags":["ReactJS","HTML","CSS","NodeJS"]},
-    "Mushroom Game Engine":{"description":"Designed to create level based multiplayer platformer games","date":"May 2022 - Now","image":"/overview/images/Fungo.png", "tags":["Java","GLSL","MacOS"]},
-    "Platform Climber":{"description":"Climb as high as you can and get your name saved on the leaderboards","date":"November 2022","image":"/overview/images/PlatformClimber.png","tags":["Rust","NodeJS","MacOS"]},
-    "KeySoundboard":{"description":"Turn your keyboard into a soundboard. Play any sound at the press of any button","date":"August 2022","image":"/overview/images/KeySoundboard.png","tags":["Swift","MacOS"]},
-    "D&D Languages":{"description":"Use AI to learn the characters of the D&D e5 languages. Upload your images to further train the AI","date":"September 2021","image":"/overview/images/D&DLanguages.png","tags":["Swift","TensorFlow","Firebase","NodeJS","IOS","Python"]},
-    "Braille Decoded":{"description":"Learn the braille alphabet","date":"August 2021","image":"/overview/images/BrailleDecoded.png","tags":["Swift","Firebase","IOS"]},
-    "Morse Decoded":{"description":"Learn to read, write, listen, and tap morse code like a master ","image":"/overview/images/MorseDecoded.png","date":"August 2021","tags":["Swift","Firebase","IOS"]},
-    "Fish's Tale":{"description":"Track what, when, and where you caught fish and watch the trends","date":"June - December 2020","image":"/overview/images/FishsTale.png","tags":["Swift","IOS"]},
-    "Copy Paste Game":{"description":"An app used to keep track of your \"Copies\" in the Copy and Paste Game","date":"May - June 2020","image":"/overview/images/CopyPaste.png","tags":["Swift","Firebase","IOS"]},
+    "This website":{"description":"Hello world!","date":"August 2022 - Now","link":"#","image":"/main/images/logoDark.png","tags":["ReactJS","HTML","CSS","NodeJS"]},
+    "Mushroom Game Engine":{"description":"Designed to create level based multiplayer platformer games","link":"/mushroom","date":"May 2022 - Now","image":"/overview/images/Fungo.png", "tags":["Java","GLSL","MacOS"]},
+    "Platform Climber":{"description":"Climb as high as you can and get your name saved on the leaderboards","link":"/climber","date":"November 2022","image":"/overview/images/PlatformClimber.png","tags":["Rust","NodeJS","MacOS"]},
+    "KeySoundboard":{"description":"Turn your keyboard into a soundboard. Play any sound at the press of any button","link":"/soundboard","date":"August 2022","image":"/overview/images/KeySoundboard.png","tags":["Swift","MacOS"]},
+    "D&D Languages":{"description":"Use AI to learn the characters of the D&D e5 languages. Upload your images to further train the AI","link":"/dnd-languages","date":"September 2021","image":"/overview/images/D&DLanguages.png","tags":["Swift","TensorFlow","Firebase","NodeJS","IOS","Python"]},
+    "Braille Decoded":{"description":"Learn the braille alphabet","date":"August 2021","link":"/braille","image":"/overview/images/BrailleDecoded.png","tags":["Swift","Firebase","IOS"]},
+    "Morse Decoded":{"description":"Learn to read, write, listen, and tap morse code like a master ","link":"/morse","image":"/overview/images/MorseDecoded.png","date":"August 2021","tags":["Swift","Firebase","IOS"]},
+    "Fish's Tale":{"description":"Track what, when, and where you caught fish and watch the trends","link":"#","date":"June - December 2020","image":"/overview/images/FishsTale.png","tags":["Swift","IOS"]},
+    "Copy Paste Game":{"description":"An app used to keep track of your \"Copies\" in the Copy and Paste Game","link":"#","date":"May - June 2020","image":"/overview/images/CopyPaste.png","tags":["Swift","Firebase","IOS"]},
 }
 
 function Home() {
@@ -24,7 +24,7 @@ function Home() {
 
     let [includedProjects, setIncludedProjects] = useState(
         Object.entries(projects).map(function ([key, value]) {
-            return (<div className={"project-wrapper"}><Project title={key} date={value['date']}  description={value['description']} image={value['image']} tags={value['tags']}/></div>);
+            return (<div className={"project-wrapper"}><Project title={key} date={value['date']} link={value['link']} description={value['description']} image={value['image']} tags={value['tags']}/></div>);
         })
     );
 
@@ -33,7 +33,7 @@ function Home() {
         Object.entries(projects).map(function ([key, value]) {
             console.log("updated project list");
             if (Object.keys(tags).every(tag => (value['tags'].includes(tag) || !tags[tag]))) {
-                objects.push(<div className={"project-wrapper"}><Project title={key} description={value['description']} image={value['image']} tags={value['tags']}/></div>);
+                objects.push(<div className={"project-wrapper"}><Project title={key} link={value['link']} description={value['description']} image={value['image']} tags={value['tags']}/></div>);
             }else{
                 objects.push(<div className={"project-wrapper project-wrapper-hidden"}></div>);
             }
--- src/components/main/overview/Project.js
+++ src/components/main/overview/Project.js
@@ -11,17 +11,19 @@ function Project(props) {
 
         return (
         <>
-            <div className="overviewProject">
-                <h1>{props.title}</h1>
-                <p className="date">{props.date}</p>
-                <img src={props.image} className="overviewProjectImage" alt={props.title+" image"}/>
-                <p className="description">{props.description}</p>
-                    <ul className="overviewProjectTags">
-                        {(props.tags).map((lang) => {
-                            return (<Tag clickable={false} active={true} lang={`${lang}`} color={`${TagsList[lang]}`}/>)
-                        })}
-                    </ul>
-            </div>
+            <a href={props.link} style={{color: "black", textDecoration: "none"}}>
+                <div className="overviewProject">
+                    <h1>{props.title}</h1>
+                    <p className="date">{props.date}</p>
+                    <img src={props.image} className="overviewProjectImage" alt={props.title+" image"}/>
+                    <p className="description">{props.description}</p>
+                        <ul className="overviewProjectTags">
+                            {(props.tags).map((lang) => {
+                                return (<Tag clickable={false} active={true} lang={`${lang}`} color={`${TagsList[lang]}`}/>)
+                            })}
+                        </ul>
+                </div>
+            </a>
         </>
     );
 }
