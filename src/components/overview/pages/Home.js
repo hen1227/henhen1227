bf3b60710b54ae4c89d965ea0a7d8200b2b67eff
Henry <henhen1227@gmail.com>
2023-07-03 19:46:49 -0400

    Updated Projects Overview Dates

--- src/components/overview/pages/Home.js
+++ src/components/overview/pages/Home.js
@@ -8,13 +8,13 @@ import SearchBar from '../SearchBar'
 // import Tag from "./Tag";
 
 const projects = {
-    "Tactico":{"description":"A fun board game you can play locally or online","date":"March 2023 - Now","link":"/tactico","image":"/images/tactico/TacticoIcon.png","tags":["ReactJS","NodeJS","SocketIO","IOS","Swift"]},
+    "Tactico":{"description":"A fun board game you can play locally or online","date":"March 2023","link":"/tactico","image":"/images/tactico/TacticoIcon.png","tags":["ReactJS","NodeJS","SocketIO","IOS","Swift"]},
     "BluetoothSerial":{"description":"An app that allows iPhones to easily interface with Arduino bluetooth HM10 chips","date":"February 2023 - Now","link":"/HM10","image":"/images/bluetooth-serial/BluetoothSerialIcon.png","tags":["Swift","IOS","C++"]},
     "This website":{"description":"A collection of all my projects","date":"August 2022 - Now","link":"#","image":"/images/main/logoTm.png","tags":["ReactJS","HTML","CSS"]},
     "Backend Server":{"description":"The backend for all my projects","date":"August 2022 - Now","link":"/api","image":"/images/main/logoBackend.png","tags":["NodeJS","Python"]},
     "Mushroom Game Engine":{"description":"Designed to create level based multiplayer platformer games","link":"/mushroom","date":"May 2022 - Now","image":"/images/overview/Fungo.png", "tags":["Java","GLSL","MacOS"]},
     "Platform Climber":{"description":"Climb as high as you can and get your name saved on the leaderboards","link":"/climber","date":"November 2022","image":"/images/overview/PlatformClimber.png","tags":["Rust","NodeJS","MacOS"]},
-    "KeySoundboard":{"description":"Turn your keyboard into a soundboard. Play any sound at the press of any button","link":"/soundboard","date":"August 2022","image":"/images/overview/KeySoundboard.png","tags":["Swift","MacOS"]},
+    "KeySoundboard":{"description":"Turn your keyboard into a soundboard. Play any sound at the press of any button","link":"/soundboard","date":"August 2022, June 2023 - Now","image":"/images/overview/KeySoundboard.png","tags":["Swift","MacOS"]},
     "D&D Languages":{"description":"Use AI to learn the characters of the D&D e5 languages. Upload your main to further train the AI","link":"/dnd-languages","date":"September 2021","image":"/images/overview/D&DLanguages.png","tags":["Swift","TensorFlow","Firebase","NodeJS","IOS","Python"]},
     "Braille Decoded":{"description":"Learn the braille alphabet","date":"August 2021","link":"/braille","image":"/images/overview/BrailleDecoded.png","tags":["Swift","Firebase","IOS"]},
     "Morse Decoded":{"description":"Learn to read, write, listen, and tap morse code like a master ","link":"/morse","image":"/images/overview/MorseDecoded.png","date":"August 2021","tags":["Swift","Firebase","IOS"]},
