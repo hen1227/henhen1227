d4734d3123b90de5b1f632fc14b273eca99c17b0
Henry <henhen1227@gmail.com>
2023-07-04 23:31:54 -0400

    Made the mods search able and alphabetized.

---
+++ src/components/minecraft/pages/ModdedHelp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const ModdedHelp = () => {
+
+    return (
+        <div>
+            <h1>Modded Help</h1>
+            <a href={"https://fabricmc.net/use/installer/"}>Fabric Installer</a>
+        </div>
+    )
+}
+
+export default ModdedHelp;
--- src/components/minecraft/pages/servers/OriginsModList.js
+++ src/components/minecraft/pages/servers/OriginsModList.js
@@ -1,115 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../../App.css';
 import './OriginsServer.css';
+import OriginsModListFile from "./OriginsModList.txt";
+
 
 const OriginsModList = () => {
+    const [sortAZ, setSortAZ] = useState(true);
+    const [search, setSearch] = useState('');
+    const [links, setLinks] = useState([]);
+
+    useEffect(() => {
+        fetch(OriginsModListFile)
+            .then(response => response.text())
+            .then(data => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(data, 'text/html');
+                const listItems = doc.querySelectorAll('li');
+
+                const linkObjects = Array.from(listItems).map(li => {
+                    const aElement = li.querySelector('a');
+                    return {
+                        url: aElement.getAttribute('href'),
+                        name: aElement.textContent
+                    };
+                });
+
+                setLinks(linkObjects);
+            })
+            .catch(err => {
+                console.error('Error: ', err);
+            });
+    }, []);
+
+    const handleSearchChange = (event) => {
+        setSearch(event.target.value);
+    }
+
+    const handleSortChange = () => {
+        setSortAZ(!sortAZ);
+    }
+
+    const getFilteredMods = () => {
+        return links.filter((mod) => mod.name.toLowerCase().includes(search.toLowerCase()))
+            .sort((a, b) => sortAZ ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name));
+    }
+
+
+
     return (
         <div className="mods-list">
-            <a href={"https://api.henhen1227.com/downloads/minecraft/originsModpack"} className={"download"}>Download Mod Pack v1</a>
-        <ul>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/yungs-better-dungeons-fabric">YUNG's Better Dungeons (Fabric) (by YUNGNICKYOUNG)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/when-dungeons-arise-fabric">When Dungeons Arise - Fabric! (by Aureljz)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/convenient-decor">Convenient Decor (by mim1q)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/farmers-delight-fabric">Farmer's Delight [Fabric] (by NewHoryzon)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/resourceful-config">Resourceful Config (by ThatGravyBoat)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/geckolib">GeckoLib (by Gecko)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/this-rocks">This Rocks! (by Motschen)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/ambientsounds">AmbientSounds 5 (by CreativeMD)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/inventory-sorting">Inventory Sorting (by Kyrptonaught)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/dramatic-doors">Dramatic Doors (by FizzWare)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/fabric-language-kotlin">Fabric Language Kotlin (by modmuss50)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/yungs-api-fabric">YUNG's API (Fabric) (by YUNGNICKYOUNG)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/extra-origins">Extra Origins (by MoriyaShiine)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/cloth-config">Cloth Config API (Fabric/Forge) (by shedaniel)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/clumps">Clumps (by Jaredlll08)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/resourceful-lib">Resourceful Lib (by ThatGravyBoat)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/pehkui">Pehkui (by Virtuoel)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/friends-and-foes-beekeeper-hut-fabric">Friends&Foes - Beekeeper Hut (Fabric) (by faboslav)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/hearth-and-home">Hearth & Home (by Starfish_Studios)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/controlling">Controlling (by Jaredlll08)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/chefs-delight-fabric">Chef's Delight [Fabric] (by redstone3game)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/simple-voice-chat">Simple Voice Chat (by henkelmax)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/yungs-bridges-fabric">YUNG's Bridges (Fabric) (by YUNGNICKYOUNG)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/repurposed-structures-fabric">Repurposed Structures (Quilt/Fabric) (by telepathicgrunt)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/forge-config-api-port-fabric">Forge Config API Port [Fabric] (by Fuzs)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/patchouli-fabric">Patchouli (Fabric/Quilt) (by williewillus)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/paladins-furniture">Paladin's Furniture (by UnlikePaladin)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/yacl">YetAnotherConfigLib (by XanderIsDev)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/friends-and-foes-flowery-mooblooms-fabric">Friends&Foes - Flowery Mooblooms (Fabric) (by faboslav)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/create-fabric">Create Fabric (by tropheusj)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/toomanyorigins">TooManyOrigins (by MerchantPug)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/do-api">[Let's Do] API  (by Cristelknight)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/comforts-fabric">Comforts (Fabric 1.16.5 - 1.19.2) (by TheIllusiveC4)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/puzzles-lib">Puzzles Lib [Forge & Fabric] (by Fuzs)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/adorn">Adorn (by Juicebus)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/more-slabs-stairs-and-walls">More Slabs, Stairs, and Walls (by Nibaru)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/fabric-api">Fabric API (by modmuss50)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/more-mob-variants">More Mob Variants (by nyuppo)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/lootr-fabric">Lootr (Fabric) (by Noobanidus)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/origins">Origins (Fabric) (by Apace)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/creativecore">CreativeCore (by CreativeMD)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/gravestones">Gravestones (by geometrically_)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/trinkets">Trinkets (Fabric) (by EmilyPloszaj)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/better-village-fabric">Better Villages - Fabric (by jtl_elisa)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/xaeros-world-map">Xaero's World Map (by xaero96)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/stoneworks">Stoneworks [Forge & Fabric] (by Fuzs)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/create-deco-fabric">Create Deco Fabric (by talrey)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/villagersplus-fabric">VillagersPlus (FABRIC) (by finallion_13)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/betterend">BetterEnd (by Quiqueck)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/immersive-armors">Immersive Armors [Fabric/Forge] (by Conczin)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/yungs-better-nether-fortresses-fabric">YUNG's Better Nether Fortresses (Fabric) (by YUNGNICKYOUNG)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/leaf-decay">Leaf Decay (by Draylar1)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/jei">Just Enough Items (JEI) (by mezz)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/litematica">Litematica (by masady)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/slice-and-dice">Create Slice & Dice (by possible_triangle)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/architectury-api">Architectury API (Fabric/Forge) (by shedaniel)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/bclib">BCLib (by Quiqueck)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/bookshelf">Bookshelf (by DarkhaxDev)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/lithium">Lithium (Fabric) (by jellysquid3_)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/mouse-tweaks">Mouse Tweaks (by YaLTeR)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/spark">spark (by Iucko)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/more-totems-of-undying">More Totems Of Undying (by cyborg_pigeon)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/traverse">Traverse (by Prospector)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/grounded-origins-fabric">Mo's Grounded Origins [FABRIC] (by mo_shark)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/cosmetic-armor-fabric">Cosmetic Armor [FABRIC] (by Apace)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/nightlights">Night Lights (by joosh_7889)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/free-cam">Freecam (Fabric/Forge) (by hashalites)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/infinite-origins-animals">Animal Origins - Infinite Origins (by Naox_95)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/xaeros-minimap">Xaero's Minimap (by xaero96)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/tree-harvester">Tree Harvester (by Serilum)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/beachparty">[Let's Do] Beachparty (by satisfy)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/mob-origins">Mob Origins (by UltrusBot)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/botany-trees">Botany Trees (by DarkhaxDev)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/yungs-better-mineshafts-fabric">YUNG's Better Mineshafts (Fabric) (by YUNGNICKYOUNG)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/rats-mischief">Rat's Mischief 🐀 (by doctor4t)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/better-advancements">Better Advancements (by way2muchnoise)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/library-ferret-fabric">Library Ferret - Fabric (by jtl_elisa)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/pack-it-up">Pack It Up! (by fzzyhmstrs)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/fabric-waystones">Fabric Waystones (by LordDeatHunter)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/terrablender-fabric">TerraBlender (Fabric) (by TheAdubbz)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/betternether">BetterNether (by Quiqueck)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/greater-eye-of-ender-fabric">Greater Eye of Ender [FABRIC] (by kwpugh)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/malilib">MaLiLib (by masady)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/zoomify">Zoomify (by XanderIsDev)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/collective">Collective (by Serilum)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/friends-and-foes">Friends&Foes (Fabric) (by faboslav)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/beautify-refabricated">Beautify: Refabricated (by Suel_ki)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/adapted-origins-an-origins-addon">Adapted Origins - An Origins Expansion (by ClarkUK)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/origins-umbrellas">Origins: Umbrellas (by Fusion_Flux)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/extra-alchemy">Extra Alchemy (by zabi94)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/creeper-overhaul">Creeper Overhaul (by joosh_7889)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/botany-pots">Botany Pots (by DarkhaxDev)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/critters-and-companions">Critters and Companions (by joosh_7889)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/owo-lib">oωo (owo-lib) (by gliscowo)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/new-life">New Life [Fabric] (by DAQEM)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/geophilic">Geophilic – Biome Overhauls (by bebebea_loste)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/war-hammers-forge-and-fabric">War Hammers (by exlinegames)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/immersive-paintings">Immersive Paintings [Fabric/Forge] (by Conczin)</a></li>
-            <li><a href="https://www.curseforge.com/minecraft/mc-mods/midnightlib">MidnightLib (by TeamMidnightDust)</a></li>
-        </ul>
-            <p>99 Total Mods</p>
-    </div>
+            <a href="https://api.henhen1227.com/downloads/minecraft/originsModpack" className="download">Download Mod Pack v1</a>
+            <br/>
+            <input type="text" value={search} onChange={handleSearchChange} placeholder="Search Mods..." />
+            <button onClick={handleSortChange}>{sortAZ ? "Sort A-Z" : "Sort Z-A"}</button>
+            <ul>
+                {getFilteredMods().map((mod, index) => (
+                    <li key={index}><a href={mod.url}>{mod.name}</a></li>
+                ))}
+            </ul>
+        </div>
     );
-}
+};
 
 export default OriginsModList;
